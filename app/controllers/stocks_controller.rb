5dbbfdc4971e82ccc670d6e2603b6b9397cc74b2
osamafish <kingfish0214@gmail.com>
2020-11-25 15:59:49 -0700

    javascript added

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,27 +2,32 @@ class StocksController < ApplicationController
 
     def search
         stock = Stock.new_lookup(params[:stock])
-
         # render json:stock 
         #--throw a json to screen 
 
-
-
-
-      if params[:stock].present?
+        if params[:stock].present?
         @stock = Stock.new_lookup(params[:stock])
         if @stock
-          render 'users/my_portfolio'
+          # render 'users/my_portfolio'
+          respond_to do |format|
+            format.js { render partial: 'users/result' }
+          end        
         else
-          flash[:alert] = "Please enter a valid symbol to search"
-          redirect_to my_portfolio_path
+          # flash[:alert] = "Please enter a valid symbol to search"
+          # redirect_to my_portfolio_path
+          respond_to do |format|
+            flash.now[:alert] = "Please enter a valid symbol to search"
+            format.js { render partial: 'users/result' }
+          end
         end    
       else
-        flash[:alert] = "Please enter a symbol to search"
-        redirect_to my_portfolio_path
+        # flash[:alert] = "Please enter a symbol to search"
+        # redirect_to my_portfolio_path
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a symbol to search"
+          format.js { render partial: 'users/result' }
+        end      
       end
-
-
 
     end
   
