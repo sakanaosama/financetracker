3dfc74282f539200aba3dea15ea8dcc501890aa4
osamafish <kingfish0214@gmail.com>
2020-12-01 00:26:29 -0700

    deploy

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,11 @@ Rails.application.routes.draw do
   # root 'application#helloworld'
   get 'search_stock', to: 'stocks#search'
   
+  get 'search_friend', to: 'users#search'
+  get 'my_friends', to: 'users#my_friends'
+  
+
+  resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_165230) do
+ActiveRecord::Schema.define(version: 2020_11_29_074218) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "friend_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -49,10 +58,14 @@ ActiveRecord::Schema.define(version: 2020_11_26_165230) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "friendships", "users"
+  add_foreign_key "friendships", "users", column: "friend_id"
   add_foreign_key "user_stocks", "stocks"
   add_foreign_key "user_stocks", "users"
 end
